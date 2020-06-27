6e71a9d6796e9b9697ea28f5234f6764cb8a2a68
ldq <ldq@PF0Z7F3X>
2020-06-27 18:46:10 +0700

    Modify code for decoder

--- decoder.py
+++ decoder.py
@@ -3,6 +3,33 @@ import torchvision.models as models
 from config import Config
 import torch
 
+class Attention(nn.Module):
+
+    def __init__(self, encoder_dim, decoder_dim, attention_dim):
+        
+        super(Attention, self).__init__()
+        self.encoder_att = nn.Linear(encoder_dim, attention_dim)  # linear layer to transform encoded image
+        self.decoder_att = nn.Linear(decoder_dim, attention_dim)  # linear layer to transform decoder's output
+        self.full_att = nn.Linear(attention_dim, 1)  # linear layer to calculate values to be softmax-ed
+        self.relu = nn.ReLU()
+        self.softmax = nn.Softmax(dim=1)  # softmax layer to calculate weights
+
+    def forward(self, encoder_out, decoder_hidden):
+
+        """
+        Forward propagation.
+        :param encoder_out: encoded images, a tensor of dimension (batch_size, num_pixels, encoder_dim)
+        :param decoder_hidden: previous decoder output, a tensor of dimension (batch_size, decoder_dim)
+        :return: attention weighted encoding, weights
+        """
+    
+        att1 = self.encoder_att(encoder_out)  # (batch_size, num_pixels, attention_dim)
+        att2 = self.decoder_att(decoder_hidden)  # (batch_size, attention_dim)
+        att = self.full_att(self.relu(att1 + att2.unsqueeze(1))).squeeze(2)  # (batch_size, num_pixels)
+        alpha = self.softmax(att)  # (batch_size, num_pixels)
+        attention_weighted_encoding = (encoder_out * alpha.unsqueeze(2)).sum(dim=1)  # (batch_size, encoder_dim)
+
+        return attention_weighted_encoding, alpha
 
 ####################
 # Attention Decoder
@@ -35,11 +62,13 @@ class Decoder(nn.Module):
         self.dropout = self.config.dropout
         
         # soft attention
-        self.enc_att = nn.Linear(2048, 512)
-        self.dec_att = nn.Linear(512, 512)
-        self.att = nn.Linear(512, 1)
-        self.relu = nn.ReLU()
-        self.softmax = nn.Softmax(dim=1)
+
+        self.attention = Attention(encoder_dim = self.encoder_dim, decoder_dim = self.decoder_dim, attention_dim = self.attention_dim)
+        # self.enc_att = nn.Linear(2048, 512)
+        # self.dec_att = nn.Linear(512, 512)
+        # self.att = nn.Linear(512, 1)
+        # self.relu = nn.ReLU()
+        # self.softmax = nn.Softmax(dim=1)
 
         # decoder layers
         self.dropout = nn.Dropout(p=self.dropout)
@@ -65,6 +94,20 @@ class Decoder(nn.Module):
             # always fine-tune embeddings (even with GloVe)
             for p in self.embedding.parameters():
                 p.requires_grad = True
+        else:
+            self.embedding = nn.Embedding(vocab_size, self.embed_dim)
+            self.embedding.weight.data.uniform_(-0.1, 0.1)
+
+    def init_hidden_state(self, encoder_out):
+        """
+        Creates the initial hidden and cell states for the decoder's LSTM based on the encoded images.
+        :param encoder_out: encoded images, a tensor of dimension (batch_size, num_pixels, encoder_dim)
+        :return: hidden state, cell state
+        """
+
+        mean_encoder_out = encoder_out.mean(dim = 1)
+        h = self.h_lin(mean_encoder_out)
+        c = self.c_lin(mean_encoder_out)
 
     def forward(self, encoder_out, encoded_captions, caption_lengths):    
         batch_size = encoder_out.size(0)
@@ -148,9 +191,7 @@ class Decoder(nn.Module):
             #print(f"Embeddings after stack: {embeddings}")
 
         # init hidden state
-        avg_enc_out = encoder_out.mean(dim=1)
-        h = self.h_lin(avg_enc_out)
-        c = self.c_lin(avg_enc_out)
+        h, c = self.init_hidden_state(encoder_out)
 
         predictions = torch.zeros(batch_size, max_dec_len, vocab_size).to(self.device)
         alphas = torch.zeros(batch_size, max_dec_len, num_pixels).to(self.device)
@@ -159,11 +200,13 @@ class Decoder(nn.Module):
             batch_size_t = sum([l > t for l in dec_len ])
             
             # soft-attention
-            enc_att = self.enc_att(encoder_out[:batch_size_t])
-            dec_att = self.dec_att(h[:batch_size_t])
-            att = self.att(self.relu(enc_att + dec_att.unsqueeze(1))).squeeze(2)
-            alpha = self.softmax(att)
-            attention_weighted_encoding = (encoder_out[:batch_size_t] * alpha.unsqueeze(2)).sum(dim=1)
+            # enc_att = self.enc_att(encoder_out[:batch_size_t])
+            # dec_att = self.dec_att(h[:batch_size_t])
+            # att = self.att(self.relu(enc_att + dec_att.unsqueeze(1))).squeeze(2)
+            # alpha = self.softmax(att)
+            # attention_weighted_encoding = (encoder_out[:batch_size_t] * alpha.unsqueeze(2)).sum(dim=1)
+
+            attention_weighted_encoding, alpha = self.attention(encoder_out[:batch_size_t], h[:batch_size_t])
         
             gate = self.sigmoid(self.f_beta(h[:batch_size_t]))
             attention_weighted_encoding = gate * attention_weighted_encoding
@@ -177,6 +220,4 @@ class Decoder(nn.Module):
             alphas[:batch_size_t, t, :] = alpha
             
         # preds, sorted capts, dec lens, attention wieghts
-        #print(f"Predictions: {predictions}")
-        #print(f"encodede captions: {encoded_captions}")
         return predictions, encoded_captions, dec_len, alphas
