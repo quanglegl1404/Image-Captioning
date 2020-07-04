cbacf876c19ff231798464212bca07b36d3cad2b
ldq <ldq@PF0Z7F3X>
2020-07-04 14:53:20 +0700

    debug for image type

--- output-capion.py
+++ output-capion.py
@@ -37,7 +37,7 @@ def load_image(image_path, transform=None):
         print('Open image')
         image = Image.open(image_path)
         image = image.resize([224, 224], Image.LANCZOS)
-        print('After resize image: {type(image)}')
+        print(f'After resize image: {type(image)}')
         if transform is not None:
             image = transform(image.to(device)).unsqueeze(0)
         
