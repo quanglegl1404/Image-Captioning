f936a11c386ee8b013752e8c67479b7aa65fd795
ldq <ldq@PF0Z7F3X>
2020-07-15 16:23:01 +0700

    train with eng baseline

--- main.py
+++ main.py
@@ -230,13 +230,13 @@ def train():
                 'model_state_dict': decoder.state_dict(),
                 'optimizer_state_dict': decoder_optimizer.state_dict(),
                 'loss': loss,
-                }, './checkpoints/decoder_epoch'+str(epoch+1)+'_july_13_eng_bert')
+                }, './checkpoints/decoder_epoch'+str(epoch+1)+'_july_15_eng_bert')
 
             torch.save({
                 'epoch': epoch,
                 'model_state_dict': encoder.state_dict(),
                 'loss': loss,
-                }, './checkpoints/encoder_epoch'+str(epoch+1)+'_july_13_eng_bert')
+                }, './checkpoints/encoder_epoch'+str(epoch+1)+'_july_15_eng_bert')
 
             print('epoch checkpoint saved')
         except Exception as ex:
