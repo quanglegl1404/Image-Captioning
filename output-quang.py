4b87a7d492361ed9b88487a41221d1b90f98a1fb
ldq <ldq@PF0Z7F3X>
2020-08-01 10:37:25 +0700

    remove get actual prediction

--- output-quang.py
+++ output-quang.py
@@ -95,9 +95,9 @@ def sample(i, imgs, caps, caplens, decoder, encoder):
     for j, p in enumerate(preds):
         print(f"decoder length j: {j},{decode_lengths[j]}")
         print(f"p: {p}")
-        pred = p[:decode_lengths[j]]
+        # pred = p[:decode_lengths[j]]
         print(f"actual prediction: {pred}")
-        pred = [w for w in pred if w not in [config.PAD, config.START, config.END]]
+        pred = [w for w in p if w not in [config.PAD, config.START, config.END]]
         temp_preds.append(pred)  # remove pads, start, and end
     preds = temp_preds
     
